9abc1a3a7831872b879e20fd1affaa7917fa928a
matthew-deluge <matthewj913@gmail.com>
2021-09-12 18:17:10 -0400

    Added Simple NavBar

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :messages
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root_to "home#index"
+  root "messages#index"
 end
